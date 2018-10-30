cbc5e9c7bf0172a19c4a7d5eed39b657438e4bac
oldone <ptsvirko@gmail.com>
2018-10-31 00:49:59 +0300

    Стреляющий корабль

---
+++ settings.py
@@ -0,0 +1,19 @@
+class Settings:
+    """Класс для хранения всех настроек игры Alien Invasion."""
+
+    def __init__(self):
+        """Инициализирует настройки игры."""
+        # Параметры экрана
+        self.screen_width = 1000
+        self.screen_height = 700
+        self.bg_color = (230, 230, 230)
+
+        # Настрой корабля
+        self.ship_speed_factor = 1.5
+
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
