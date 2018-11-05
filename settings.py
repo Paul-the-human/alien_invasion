b4b88aeb6ad6f9093fca31bd59d9adc94106af71
oldone <ptsvirko@gmail.com>
2018-11-05 08:04:58 +0300

    Корабль с пришельцами

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ class Settings:
 
         # Настрой корабля
         self.ship_speed_factor = 1.5
+        self.ship_limit = 3
 
         # Параметры пули
         self.bullet_speed_factor = 1
@@ -17,3 +18,9 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
+
+        # Настройки пришельцев
+        self.alien_speed_factor = 3
+        self.fleet_drop_speed = 4
+        # fleet_direction = 1 обозначает движение вправо; а -1 - влево.
+        self.fleet_direction = 1
